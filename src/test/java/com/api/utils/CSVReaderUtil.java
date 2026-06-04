082f5192622c2b7644bad5afde079e1540ddb388
Divya <divyananda0410@gmail.com>
2026-06-04 19:17:52 +0400

    Refractored the project structure.
    CSVReaderUtil :load the csv file and map it to UserBean
    
    UserBean: Bean Class for CSV mapping and will be used by DataProviders.
    
    moved the testData from src/main/resources to src/test/resources

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -1,22 +1,23 @@
-package com.demo.csv;
+package com.api.utils;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
+import com.dataproviders.api.bean.UserBean;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvException;
 
-public class ReadCSVFile_MapToPOJO {
-
-	public static void main(String[] args) throws IOException, CsvException {
+public class CSVReaderUtil {
+	
+	
+	private CSVReaderUtil() {
 		
-		InputStream inputStream=Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
+	}
+
+	public static void loadCSV(String pathOfCSVFile) {
+		InputStream inputStream=Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
 		InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
 		
 		CSVReader csvReader=new CSVReader(inputStreamReader); 
@@ -30,5 +31,4 @@ public class ReadCSVFile_MapToPOJO {
 		System.out.println(list);
 		System.out.println(list.get(0).getUsername());
 	}
-
 }
