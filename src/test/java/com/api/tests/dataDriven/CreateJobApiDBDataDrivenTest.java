5a9232b5cca87e90bc9f7a860691974c8ae62e81
Divya <divyananda0410@gmail.com>
2026-07-10 14:24:04 +0400

    Completed the DataDrivenTest for create job API with DB test data

---
+++ src/test/java/com/api/tests/dataDriven/CreateJobApiDBDataDrivenTest.java
@@ -0,0 +1,34 @@
+package com.api.tests.dataDriven;
+
+import static com.api.utils.SpecUtil.requestSpecWithAuth;
+import static com.api.utils.SpecUtil.responseSpec_OK;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.startsWith;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.api.constants.Role;
+import com.api.request.model.CreateJobPayload;
+
+public class CreateJobApiDBDataDrivenTest {
+
+	private CreateJobPayload createJobPayload;
+
+	@Test(description = "Verify if create job api ia able to create inwarrenty job", groups = { "api", "regression",
+			"datadriven","faker" },
+			dataProviderClass  = com.dataproviders.DataProviderUtil.class,
+			dataProvider = "CreateJobAPIDBDataProvider")
+	public void createJobApiTest(CreateJobPayload createJobPayload) throws IOException {
+		         given().spec(requestSpecWithAuth(Role.FD, createJobPayload)).when().post("/job/create").then()
+				.spec(responseSpec_OK())
+				.body(matchesJsonSchemaInClasspath("responseSchema/createJobApiResponseSchema.json"))
+				.body("message", equalTo("Job created successfully. ")).body("data.mst_service_location_id", equalTo(1))
+				.body("data.job_number", startsWith("JOB_"));
+
+	}
+
+}
---
+++ src/test/java/com/database/dao/CreateJobPayloadDataDAO.java
@@ -0,0 +1,94 @@
+package com.database.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.database.DatabaseManager;
+import com.dataproviders.api.bean.CreateJobBean;
+
+public class CreateJobPayloadDataDAO {
+
+	private static String SQL_QUERY = """
+						select tc.first_name,tc.last_name,tc.mobile_number,tc.mobile_number_alt,
+			tc.email_id,tc.email_id_alt,tca.flat_number ,tca.apartment_name ,tca.street_name ,
+			tca.landmark ,tca.area ,tca.pincode ,tca.country ,tca.state,
+			tcp.dop,
+			tcp.serial_number,
+			tcp.imei1,
+			tcp.imei2,
+			tcp.popurl,
+			tcp.mst_model_id,
+			tjh.mst_service_location_id,
+			tjh.mst_platform_id,
+			tjh.mst_warrenty_status_id,
+			tjh.mst_oem_id,
+			mjp.mst_problem_id,
+			mjp.remark
+			from tr_customer tc
+			inner join
+			tr_customer_address tca
+			on tc.tr_customer_address_id = tca.id
+			inner JOIN tr_customer_product tcp
+			on tc.id =tcp.tr_customer_id
+			inner JOIN tr_job_head tjh
+			on tc.id=tjh.tr_customer_id
+			inner join map_job_problem mjp
+			on tjh.id =mjp.tr_job_head_id
+			limit 5
+
+						""";
+	
+	public static List<CreateJobBean> getCreateJobPayloadData() {
+		Connection connection=null;
+		Statement statement;
+		ResultSet resultSet;
+		List<CreateJobBean> beanList=new ArrayList<CreateJobBean>();
+		try {
+			connection=DatabaseManager.getConnection();
+			statement=connection.createStatement();
+			resultSet=statement.executeQuery(SQL_QUERY);
+			while(resultSet.next()) {
+				CreateJobBean createJobBean=new CreateJobBean();
+				createJobBean.setMst_service_location_id(resultSet.getString("mst_service_location_id"));
+				createJobBean.setMst_platform_id(resultSet.getString("mst_platform_id"));
+				createJobBean.setMst_warrenty_status_id(resultSet.getString("mst_warrenty_status_id"));
+				createJobBean.setMst_oem_id(resultSet.getString("mst_oem_id"));
+				createJobBean.setCustomer__first_name(resultSet.getString("first_name"));
+				createJobBean.setCustomer__last_name(resultSet.getString("last_name"));
+				createJobBean.setCustomer__mobile_number(resultSet.getString("mobile_number"));
+				createJobBean.setCustomer__mobile_number_alt(resultSet.getString("mobile_number_alt"));
+				createJobBean.setCustomer__email_id(resultSet.getString("email_id"));
+				createJobBean.setCustomer__email_id_alt(resultSet.getString("email_id_alt"));
+				createJobBean.setCustomer_address__flat_number(resultSet.getString("flat_number"));
+				createJobBean.setCustomer_address__apartment_name(resultSet.getString("apartment_name"));
+				createJobBean.setCustomer_address__street_name(resultSet.getString("street_name"));
+				createJobBean.setCustomer_address__landmark(resultSet.getString("landmark"));
+				createJobBean.setCustomer_address__area(resultSet.getString("area"));
+				createJobBean.setCustomer_address__pincode(resultSet.getString("pincode"));
+				createJobBean.setCustomer_address__country(resultSet.getString("country"));
+				createJobBean.setCustomer_address__state(resultSet.getString("state"));
+				createJobBean.setCustomer_product__dop(resultSet.getString("dop"));
+				createJobBean.setCustomer_product__serial_number(resultSet.getString("serial_number"));
+				createJobBean.setCustomer_product__imei1(resultSet.getString("imei1"));
+				createJobBean.setCustomer_product__imei2(resultSet.getString("imei2"));
+				createJobBean.setCustomer_product__popurl(resultSet.getString("popurl"));
+				createJobBean.setCustomer_product__mst_model_id(resultSet.getString("mst_model_id"));
+				createJobBean.setProblems__id(resultSet.getString("mst_problem_id"));
+				createJobBean.setProblems__remark(resultSet.getString("remark"));
+				createJobBean.setCustomer_product__product_id("1");
+				beanList.add(createJobBean);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return beanList;
+		
+	}
+
+}
